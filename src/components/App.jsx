4bfe23de8fadec166142ffa7432035b378a78317
Oleg Palashynskyi <palah@ukr.net>
2022-10-29 12:58:48 +0300

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import Filter from './Filter/Filter';
 import css from './App.module.css';
 import { Route, Routes } from 'react-router-dom';
 import RegisterView from 'redux/views/RegisterView/RegisterView';
-import { Layout } from 'redux/views/Layout';
+import { Layout } from 'redux/views/Layout/Layout';
 import HomeView from 'redux/views/HomeView/HomeView';
 import LoginView from 'redux/views/LoginView/LoginView';
 
@@ -17,10 +17,8 @@ const App = () => {
           <Route index element={<HomeView />} />
           <Route path="/contacts " element={<ContactsList />} />
         </Route>
-        {/* <Route> */}
         <Route path="/register" element={<RegisterView />} />
         <Route path="/login" element={<LoginView />} />
-        {/* </Route> */}
       </Routes>
       <ContactForm />
       <Filter />
