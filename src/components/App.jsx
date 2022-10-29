14f2588241890074b510ee41c642444b9b814389
Oleg Palashynskyi <palah@ukr.net>
2022-10-29 12:01:42 +0300

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,16 @@ import css from './App.module.css';
 import { Route, Routes } from 'react-router-dom';
 import RegisterView from 'redux/views/RegisterView';
 import { Layout } from 'redux/views/Layout';
+import HomeView from 'redux/views/HomeView';
 
 const App = () => {
   return (
     <div className={css.appWrapper}>
       <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route path="/register" element={<RegisterView />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomeView />} />
+          <Route path="/register" element={<RegisterView />} />
+        </Route>
       </Routes>
       <ContactForm />
       <Filter />
--- src/redux/views/Layout.js
+++ src/redux/views/Layout.js
@@ -1,6 +1,9 @@
+import { Outlet } from 'react-router-dom';
+
 export const Layout = () => {
   return (
     <div>
+      <Outlet />
       <h2>Layout</h2>
     </div>
   );
