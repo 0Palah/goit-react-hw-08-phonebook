76518d85f439ba11c0b5f35b3c7f55505659ca5e
Oleg Palashynskyi <palah@ukr.net>
2022-10-31 22:04:29 +0200

    w

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -4,13 +4,27 @@ import Navigation from 'components/Navigation/Navigation';
 import UserMenu from 'components/UserMenu/UserMenu';
 import AuthNav from 'components/AuthNav/AuthNav';
 import css from './AppBar.module.css';
+import { AppBar, Box, SvgIcon, Toolbar, Typography } from '@mui/material';
+import { Contacts } from '@mui/icons-material';
 
-export default function AppBar() {
+export default function AppBarComp() {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   return (
-    <header className={css.header}>
-      <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </header>
+    <AppBar>
+      {/* <SvgIcon>
+        <Contacts />
+      </SvgIcon> */}
+      <Toolbar>
+        <Box sx={{ flexGrow: 1 }}>
+          <Navigation />
+        </Box>
+
+        {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      </Toolbar>
+    </AppBar>
+    // <header className={css.header}>
+    //   <Navigation />
+    //   {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    // </header>
   );
 }
